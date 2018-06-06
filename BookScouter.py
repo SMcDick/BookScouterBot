4c4b3400cf80d8ed1c118fed39d5ecc1ef10de90
SalahAssana <salahmassana@gmail.com>
2018-06-06 03:09:04 +0100

    Update formatting to make output more readable.

--- BookScouter.py
+++ BookScouter.py
@@ -5,6 +5,9 @@ from sys import argv, exit
 from json import loads
 from urllib.request import urlopen
 from urllib.error import URLError
+from book import Book
+
+buyers = {}
 
 def checkPrices(isbn):
 
@@ -14,47 +17,69 @@ def checkPrices(isbn):
 
     try:
         req = urlopen(url)
-    # If there is an error print the URL, show error code and reason. 
-    except URLError:
+    
+        # Continue if the request went through
+        data = loads( req.read() )
+    
+        # This will print the entire dictionary in a readable format
+        #print (json.dumps(data, ensure_ascii=False, indent=4, sort_keys=True))
+    
+        # The response is a dictionary
+        data = data['data']
+    
+        # If the search doesn't find any results the Book field will be null.
+        if data['Book'] is not None:
+    
+            # The results are always in decedning order of price so if the first isn't great than 0 none of them will be.
+            if data['Prices'][0]['Price'] > 0:
+            
+                #print('---------------------------------') 
+                #print(data['Book']['Title'] + "-" + data['Book']['Isbn13'])
+    
+                for item in data['Prices']:
 
+                    if item['Price'] > 0:
+                        #print(item['Vendor']['Name'] + " " + str(item['Price']))
+                        
+                        # Generate a book object to hold all the necessary information.
+                        book = Book(data['Book']['Isbn13'], data['Book']['Title'], item['Price'])
+                        
+                        # If this is a new vendor then add them to the dictionary. Otherwise just append the book to the list.
+                        if (item['Vendor']['Name'] in buyers): 
+                            buyers[item['Vendor']['Name']].append(book)
+                        else:
+                            buyers[item['Vendor']['Name']] = []
+                            buyers[item['Vendor']['Name']].append(book)
+
+    # If there is an error print the URL, show error code and reason. 
+    except URLError as e:
         print("This URL failed:", url)
-        print("HTTP ERROR:", req.status_code)
-        print("REASON:", req.reason)
+        print("HTTP ERROR:", e.code)
+        print("REASON:", e.reason)
         return
 
-    # Continue if the request went through
-    data = loads( req.read() )
-
-    # This wiill print the entire dictionary in a readable format
-    #print (json.dumps(data, ensure_ascii=False, indent=4, sort_keys=True))
-
-    # The response is a dictionary
-    data = data['data']
-
-    # If the search doesn't find any results the Book field will be null.
-    if data['Book'] is not None:
-    
-        # The results are always in decedning order of price so if the first isn't great than 0 none of them will be.
-        if data['Prices'][0]['Price'] > 0:
-            
-            print('---------------------------------')
-            
-            print(data['Book']['Title'] + "-" + data['Book']['Isbn13'])
-
-            for item in data['Prices']:
-                if item['Price'] > 0:
-                    print(item['Vendor']['Name'] + " " + str(item['Price']))
-    
-    else:
-    
-        print('---------------------------------')
-        print ('Invalid ISBN')
-
 if __name__=="__main__":
-
+    counter = 0
     # Read in the file of ISBN numbers
     with open('isbnList.txt', 'r') as isbnList:
         for isbn in isbnList:
+            #if counter == 10:
+            #    break
             checkPrices(isbn)
             sleep(5)
+            #counter += 1
+        
+        totalPrice = 0
+
+        for vendor, booksBought in buyers.items():
+            print (vendor)
+            for book in booksBought:
+                print ("Book:", book.title, book.isbn, book.price),
+                totalPrice += book.price
+            
+            print ("Total Price: " + str(totalPrice))
+            totalPrice = 0
+            print ("---------------------------------")
+
+        print("Finished")
 
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BookScouterBot
+
+![Book Scouter Logo](bks_logo.svg "Title")
+
 BookScouter.com is a book buyback aggregator which searches multipule websites to help you find the best price for your book. However, the price of the book you are trying to sell can flucuate (e.g. a book that sells for $0 in January could sell for $23 in March). Manually typing in the ISBNs of all the books you want to sell is a tedious and time-consuming process so I have written a bot which reads the ISBNs from a file and returns the available offers.
